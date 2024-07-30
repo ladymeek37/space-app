62d43c34ca5efa697fe228e190fe8343df356c49
Lady Meek <ladyjmeek@gmail.com>
2024-07-30 10:45:22 -0700

    mobile nav toggle

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,1 +1,25 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+
+// when someone clicks the hamburger button
+navToggle.addEventListener("click",() => {
+    
+    const visiblity = nav.getAttribute("data-visible");
+    // if the nav is closed, open it
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+    // if the nav is open, close it
+    } else {
+        nav.setAttribute("data-visible", false);
+    }
+    
+    
+    
+    console.log(visiblity);
+
+})
+
+
+
 
